3ec3bc43dbd0661d125c465f88479f3f74cf7f02
Stephen Hart <swhart@traclabs.com>
2023-03-29 16:26:05 -0500

    updated joint conversion script

--- brash_demo/brash_demo/joint_state_convert.py
+++ brash_demo/brash_demo/joint_state_convert.py
@@ -2,8 +2,8 @@ import rclpy
 from rclpy.node import Node
 
 from sensor_msgs.msg import JointState
-from cfs_msgs.msg import ROBOTSIMHKTlm
-from cfs_msgs.msg import RobotSimCmd
+from cfe_msgs.msg import RobotSimHkTlmt
+from cfe_msgs.msg import RobotSimJointCmdt
 
 
 class JointStateConverter(Node):
@@ -11,10 +11,10 @@ class JointStateConverter(Node):
     def __init__(self):
         super().__init__('joint_state_convert')
         self.js_publisher = self.create_publisher(JointState, 'joint_states', 10)
-        self.jc_publisher = self.create_publisher(RobotSimCmd, '/robot_sim_cmd', 10)
+        self.jc_publisher = self.create_publisher(RobotSimJointCmdt, '/groundsystem/robot_sim_cmd', 10)
         self.js_subscription = self.create_subscription(
-            ROBOTSIMHKTlm,
-            'cfe_robot_sim_hk_tlm',
+            RobotSimHkTlmt,
+            '/groundsystem/robot_sim_hk_tlm',
             self.cfs_callback,
             10)
 
@@ -29,8 +29,8 @@ class JointStateConverter(Node):
 
     def cfs_callback(self, msg):
         self.get_logger().info('got new cFS joint telemetry')
-        # self.get_logger().info(msg)
-        print(msg)
+        # self.get_logger().info(str(msg))
+        # print(msg)
         js = JointState()
         js.header.stamp = self.get_clock().now().to_msg()
         # js.header.frame_id = "iss"
@@ -41,30 +41,31 @@ class JointStateConverter(Node):
         js.name.append("joint4")
         js.name.append("joint5")
         js.name.append("joint6")
-        js.position.append(msg.joint0)
-        js.position.append(msg.joint1)
-        js.position.append(msg.joint2)
-        js.position.append(msg.joint3)
-        js.position.append(msg.joint4)
-        js.position.append(msg.joint5)
-        js.position.append(msg.joint6)
+        js.position.append(msg.payload.state.joint0)
+        js.position.append(msg.payload.state.joint1)
+        js.position.append(msg.payload.state.joint2)
+        js.position.append(msg.payload.state.joint3)
+        js.position.append(msg.payload.state.joint4)
+        js.position.append(msg.payload.state.joint5)
+        js.position.append(msg.payload.state.joint6)
 
-        print(js)
+        # print(js)
         # self.get_logger().info(js)
         self.js_publisher.publish(js)
 
     def jc_callback(self, msg):
         self.get_logger().info('got new cFS joint command')
+        # self.get_logger().info(str(msg))
 
-        cmd = RobotSimCmd()
-        cmd.cmd_id = 1
-        cmd.robot_sim_set_joints_cc.joint0 = msg.position[0]
-        cmd.robot_sim_set_joints_cc.joint1 = msg.position[1]
-        cmd.robot_sim_set_joints_cc.joint2 = msg.position[2]
-        cmd.robot_sim_set_joints_cc.joint3 = msg.position[3]
-        cmd.robot_sim_set_joints_cc.joint4 = msg.position[4]
-        cmd.robot_sim_set_joints_cc.joint5 = msg.position[5]
-        cmd.robot_sim_set_joints_cc.joint6 = msg.position[6]
+        cmd = RobotSimJointCmdt()
+        cmd.cmd_header.sec.function_code = 1
+        cmd.joint0 = msg.position[0]
+        cmd.joint1 = msg.position[1]
+        cmd.joint2 = msg.position[2]
+        cmd.joint3 = msg.position[3]
+        cmd.joint4 = msg.position[4]
+        cmd.joint5 = msg.position[5]
+        cmd.joint6 = msg.position[6]
 
         self.jc_publisher.publish(cmd)
 
