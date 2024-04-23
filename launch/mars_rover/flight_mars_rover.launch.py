8e4606dc8f3da92386511e335411d1b0cbb8f97e
Ana C. Huaman Quispe <ana@traclabs.com>
2024-04-23 12:38:01 -0500

    Fixed name for topic in mars odometry

--- launch/mars_rover/flight_mars_rover.launch.py
+++ launch/mars_rover/flight_mars_rover.launch.py
@@ -2,8 +2,9 @@ import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 ARGUMENTS = [
     DeclareLaunchArgument('twist_out_cfs', default_value='rover_app_send_robot_command',
@@ -12,15 +13,26 @@ ARGUMENTS = [
                           description='topic name for odom cfs tlm'),
     DeclareLaunchArgument('twist_out', default_value='/cmd_vel',
                           description='topic name for twist topic to robot'),
-    DeclareLaunchArgument('odom_in', default_value='/odom',
+    DeclareLaunchArgument('odom_in', default_value='/model/curiosity_mars_rover/odometry',
                           description='topic name for odom topic from robot'),                          
 ]
 
 #####################################
 def generate_launch_description():
 
+  # Start the twist_odom_converter
+  launch_mars_rover = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([
+          PathJoinSubstitution([
+              get_package_share_directory('mars_rover'), 
+              'launch', 'mars_rover.launch.py'
+          ])
+      ])              
+  )
+  
+
   toc_convert = Node(
-          package='rover_demo',
+          package='brash_application_tools',
           executable='flight_twist_odom_convert',
           name='flight_twist_odom_convert',
           output='screen',
@@ -32,6 +44,7 @@ def generate_launch_description():
           ]) 
           
   ld = LaunchDescription(ARGUMENTS)
+  ld.add_action(launch_mars_rover)
   ld.add_action(toc_convert)
   return ld
   
